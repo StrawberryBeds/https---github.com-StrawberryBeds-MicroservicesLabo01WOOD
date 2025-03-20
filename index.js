f81d38954819c074565becd5ca8babe109ce0d5e
StrawberryBeds <samuel.l.wood@gmail.com>
2025-03-20 19:52:12 -0400

    Correction du path et confirmation du lecture du data.json

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello World! Node is present!")
+console.log("Hello World! Node is present!");
+
+const path = require('path');
+const fs = require('fs');
+const filePath = '/Users/samuelwood/Documents/Microservices/MicroservicesLabo01WOOD/config/data.json';
+console.log(path.isAbsolute('/Users/samuelwood/Documents/Microservices/MicroservicesLabo01WOOD/config/data.json'));  // true​
+console.log(filePath);
+
+
+const fileContent = fs.readFileSync(filePath, 'utf8');
+
+const jsonData = JSON.parse(fileContent);
+
+console.log(jsonData);
