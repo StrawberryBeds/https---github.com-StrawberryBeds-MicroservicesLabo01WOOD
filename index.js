b3cf4afd4946279bec7d53214df19def9d1bddc5
StrawberryBeds <samuel.l.wood@gmail.com>
2025-03-20 21:10:51 -0400

    Modification du appel data.json

--- index.js
+++ index.js
@@ -2,15 +2,20 @@ console.log("Hello World! Node is present!");
 
 const path = require('path');
 const fs = require('fs');
-const filePath = '/Users/samuelwood/Documents/Microservices/MicroservicesLabo01WOOD/config/data.json';
-console.log(path.isAbsolute('/Users/samuelwood/Documents/Microservices/MicroservicesLabo01WOOD/config/data.json'));  // true​
-console.log(filePath);
+// const filePath = '/Users/samuelwood/Documents/Microservices/MicroservicesLabo01WOOD/config/data.json';
+// console.log(path.isAbsolute('/Users/samuelwood/Documents/Microservices/MicroservicesLabo01WOOD/config/data.json'));  // true​
+// console.log(filePath);
 
-fs.readFileSync(filePath, 'utf-8',(err, data) => {
-    if (err) throw error;
-    console.log(data);
-})
-process.on('uncaughtException', err => {
-    console.error(`There was an uncaught error:  ${err}`);
-    process.exit(1)
-})
+const fileContent = fs.readFileSync(path.join(__dirname, 'config', 'data.json'), 'utf8');
+const jsonData = JSON.parse(fileContent);
+
+console.log(jsonData);
+
+// fs.readFileSync(filePath, 'utf-8',(err, data) => {
+//     if (err) throw error;
+//     console.log(data);
+// })
+// process.on('uncaughtException', err => {
+//     console.error(`There was an uncaught error:  ${err}`);
+//     process.exit(1)
+// })
